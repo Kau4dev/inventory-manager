6c3522579547c1ecf1cd9708366553d0f4571d25
kau4dev <kauavictordev7@gmail.com>
2026-02-06 15:26:46 -0300

    feat: add mapping for ProductMaterialDTO in ProductMapper

--- backend/src/main/java/com/projedata/inventory_manager/mapper/ProductMapper.java
+++ backend/src/main/java/com/projedata/inventory_manager/mapper/ProductMapper.java
@@ -3,8 +3,11 @@ package com.projedata.inventory_manager.mapper;
 import com.projedata.inventory_manager.dto.product.ProductCreatedDTO;
 import com.projedata.inventory_manager.dto.product.ProductUpdateDTO;
 import com.projedata.inventory_manager.dto.product.ProductViewDTO;
+import com.projedata.inventory_manager.dto.productMaterial.ProductMaterialDTO;
 import com.projedata.inventory_manager.model.Product;
+import com.projedata.inventory_manager.model.ProductMaterial;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
@@ -14,10 +17,15 @@ public interface ProductMapper {
 
     ProductCreatedDTO toCreatedDTO(Product product);
 
+    @Mapping(target = "materials", source = "materials")
     ProductViewDTO toViewDTO(Product product);
 
     ProductUpdateDTO toUpdateDTO(Product product);
 
     void updateFromDto(ProductUpdateDTO dto, @MappingTarget Product entity);
+
+    @Mapping(target = "materialId", source = "material.id")
+    @Mapping(target = "requiredQuantity", source = "requiredQuantity")
+    ProductMaterialDTO toProductMaterialDTO(ProductMaterial productMaterial);
 
 }
